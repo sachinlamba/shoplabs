a9dab2f574504bafe886433ed17eabed756122e5
lambag <lamba92sachin@gmail.com>
2018-02-11 02:31:57 +0530

    maker lab deployment as shoplabs on heroku

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <iframe
+        width="350"
+        height="430"
+        src="https://console.dialogflow.com/api-client/demo/embedded/b391e62e-588b-48a0-b250-25690d5b8c41">
+      </iframe>
+      </div>
+    );
+  }
+}
+
+export default App;
