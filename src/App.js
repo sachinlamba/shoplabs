a02576a9dc1e50f0b13c1f88782cb367a2931b80
Sachin Lamba <sachin.lamba@moldedbits.com>
2018-02-13 14:58:09 +0530

    Update App.js
    user says list added.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <iframe
-        width="350"
-        height="430"
-        src="https://console.dialogflow.com/api-client/demo/embedded/b391e62e-588b-48a0-b250-25690d5b8c41">
-      </iframe>
+        <div className="iframe-div">
+          <iframe className="iframe-df"
+            src="https://console.dialogflow.com/api-client/demo/embedded/b391e62e-588b-48a0-b250-25690d5b8c41">
+          </iframe>
+        </div>
+        <div className="layoutResizer"></div>
+        <div className="userSays">
+          <div style={{paddingTop: "15px"}} className="list-sent">Say like this :</div>
+          <ul className="list-para">
+            <li className="list-sent">show available products</li>
+            <li className="list-sent">Register me as ****@***.*** with password *****</li>
+            <li className="list-sent">login me by  ****@***.*** and ********</li>
+            <li className="list-sent">logout me</li>
+          </ul>
+        </div>
       </div>
     );
   }
